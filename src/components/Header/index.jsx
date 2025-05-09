e267fc32378454c7b05fb7b516f20b76ccecd808
nessim-cmd <bennasrnessim@outlook.com>
2025-05-09 11:28:52 +0100

    preparation

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div className='top-strip py-2'>
+        <div className='container'>
+            <div className='flex items-center justify-between'>
+                <div className='col1 w-[50%]'>
+                    <p className='text-[14px]'>Get up to 50% off new season styles, limited time only</p>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
